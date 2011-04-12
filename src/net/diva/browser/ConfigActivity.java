dd9cc284e53536b1a55d8bdc947f3ceaabc540e8
DdN Browser <ddnbrowser@gmail.com>
2011-04-12 13:14:03 +0000

    ConfigActivity のレイアウトとして config.xml の代わりに basic_list.xml を設定するように変更

--- src/net/diva/browser/ConfigActivity.java
+++ src/net/diva/browser/ConfigActivity.java
@@ -25,7 +25,7 @@ public class ConfigActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.config);
+		setContentView(R.layout.basic_list);
 
 		m_adapter = new ConfigAdapter(this,
 				new ConfigCategory(getText(R.string.category_player)),

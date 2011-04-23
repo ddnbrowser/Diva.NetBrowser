65e73753cb0ef78f2932a5b7a9aaadc3140294cf
DdN Browser <ddnbrowser@gmail.com>
2011-04-23 03:26:01 +0000

    スキン一覧を展開リストに置き換え

--- src/net/diva/browser/settings/SkinListActivity.java
+++ src/net/diva/browser/settings/SkinListActivity.java
@@ -3,7 +3,12 @@ package net.diva.browser.settings;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.diva.browser.DdN;
 import net.diva.browser.R;
@@ -11,7 +16,7 @@ import net.diva.browser.db.LocalStore;
 import net.diva.browser.model.SkinInfo;
 import net.diva.browser.service.ServiceClient;
 import net.diva.browser.service.ServiceTask;
-import android.app.ListActivity;
+import android.app.ExpandableListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
@@ -22,13 +27,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SkinListActivity extends ListActivity {
+public class SkinListActivity extends ExpandableListActivity {
+	private static final Pattern RE_NAME = Pattern.compile("(.+)(\\[.+\\])");
+
+	private class Skin {
+		int id;
+		String name;
+		boolean hasPurchased = false;
+		List<Variant> variants = new ArrayList<Variant>();
+	}
+
+	private class Variant {
+		String name;
+		SkinInfo detail;
+	}
+
+	private Map<String, Skin> m_skins;
+	private List<Skin> m_purchased;
+
 	private SkinAdapter m_adapter;
 	private LocalStore m_store;
 
@@ -38,14 +60,23 @@ public class SkinListActivity extends ListActivity {
 		setContentView(R.layout.skin_list);
 		m_store = LocalStore.instance(this);
 
+		m_skins = reconstruct(m_store.loadSkins());
+		m_purchased = filterPurchased(m_skins.values());
+
 		m_adapter = new SkinAdapter(this);
-		m_adapter.setSkins(m_store.loadSkins());
+		refresh(savedInstanceState != null && savedInstanceState.getBoolean("showAll"));
 		setListAdapter(m_adapter);
 	}
 
 	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		SkinInfo skin = m_adapter.getItem(position);
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putBoolean("showAll", m_adapter.getSkins() != m_purchased);
+	}
+
+	@Override
+	public boolean onChildClick(ExpandableListView parent, View view, int group, int child, long id) {
+		SkinInfo skin = m_adapter.getChild(group, child).detail;
 		if (skin.purchased) {
 			Intent intent = getIntent();
 			intent.putExtra("id", skin.id);
@@ -60,6 +91,7 @@ public class SkinListActivity extends ListActivity {
 			intent.putExtra("group_id", skin.group_id);
 			startActivityForResult(intent, R.id.item_confirm_buying);
 		}
+		return true;
 	}
 
 	@Override
@@ -67,7 +99,7 @@ public class SkinListActivity extends ListActivity {
 		switch (requestCode) {
 		case R.id.item_confirm_buying:
 			if (resultCode == RESULT_OK) {
-				SkinInfo skin = m_adapter.getSkinInfo(data.getStringExtra("id"), data.getStringExtra("group_id"));
+				SkinInfo skin = getSkinInfo(data.getStringExtra("id"), data.getStringExtra("group_id"));
 				new BuyTask().execute(skin);
 			}
 			break;
@@ -89,7 +121,7 @@ public class SkinListActivity extends ListActivity {
 			new SkinDownloader().execute();
 			break;
 		case R.id.item_toggle_show_all:
-			m_adapter.toggleShowAll();
+			refresh(!isShowAll());
 			break;
 		default:
 			return super.onOptionsItemSelected(item);
@@ -98,76 +130,183 @@ public class SkinListActivity extends ListActivity {
 		return true;
 	}
 
-	private static class SkinAdapter extends BaseAdapter {
+	private boolean isShowAll() {
+		return m_adapter.getSkins() != m_purchased;
+	}
+
+	private void refresh(boolean showAll) {
+		if (showAll)
+			m_adapter.setSkins(new ArrayList<Skin>(m_skins.values()));
+		else
+			m_adapter.setSkins(m_purchased);
+	}
+
+	private Map<String, Skin> reconstruct(List<SkinInfo> skins) {
+		int id = 0;
+		Map<String, Skin> map = new TreeMap<String, Skin>();
+		for (SkinInfo s: skins) {
+			String sName = s.name;
+			String vName;
+			Matcher m = RE_NAME.matcher(sName);
+			if (m.matches()) {
+				sName = m.group(1);
+				vName = m.group(2);
+			}
+			else {
+				vName = getString(R.string.variant_normal);
+			}
+
+			Variant variant = new Variant();
+			variant.name = vName;
+			variant.detail = s;
+
+			Skin skin = map.get(s.group_id);
+			if (skin == null) {
+				skin = new Skin();
+				skin.id = id++;
+				skin.name = sName;
+				map.put(s.group_id, skin);
+			}
+			skin.hasPurchased |= s.purchased;
+			skin.variants.add(variant);
+		}
+		return map;
+	}
+
+	private List<Skin> filterPurchased(Collection<Skin> skins) {
+		List<Skin> purchased = new ArrayList<Skin>(skins.size());
+		for (Skin s: skins) {
+			if (!s.hasPurchased)
+				continue;
+
+			Skin skin = new Skin();
+			skin.id = s.id;
+			skin.name = s.name;
+			for (Variant v: s.variants) {
+				if (v.detail.purchased)
+					skin.variants.add(v);
+			}
+			purchased.add(skin);
+		}
+		return purchased;
+	}
+
+	SkinInfo getSkinInfo(String id, String group_id) {
+		Skin skin = m_skins.get(group_id);
+		if (skin == null)
+			return null;
+
+		for (Variant variant: skin.variants) {
+			SkinInfo si = variant.detail;
+			if (si.id.equals(id))
+				return si;
+		}
+
+		return null;
+	}
+
+	private static class SkinAdapter extends BaseExpandableListAdapter {
 		Context m_context;
-		List<SkinInfo> m_skins;
-		List<SkinInfo> m_purchased;
-		boolean m_showAll = false;
+		List<Skin> m_skins;
 
 		SkinAdapter(Context context) {
-			super();
 			m_context = context;
 		}
 
-		void setSkins(List<SkinInfo> skins) {
+		void setSkins(List<Skin> skins) {
 			m_skins = skins;
-			List<SkinInfo> purchased = new ArrayList<SkinInfo>();
-			for (SkinInfo skin: skins) {
-				if (skin.purchased)
-					purchased.add(skin);
-			}
-			m_purchased = purchased;
-			notifyDataSetChanged();
+			if (m_skins.isEmpty())
+				notifyDataSetInvalidated();
+			else
+				notifyDataSetChanged();
 		}
 
-		void toggleShowAll() {
-			m_showAll = !m_showAll;
-			notifyDataSetChanged();
+		List<Skin> getSkins() {
+			return m_skins;
 		}
 
-		SkinInfo getSkinInfo(String id, String group_id) {
-			for (SkinInfo skin: m_skins) {
-				if (skin.id.equals(id) && skin.group_id.equals(group_id))
-					return skin;
-			}
-			return null;
+		public Variant getChild(int group, int child) {
+			return m_skins.get(group).variants.get(child);
 		}
 
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View view = convertView;
-			if (view == null) {
-				LayoutInflater inflater = LayoutInflater.from(m_context);
-				view = inflater.inflate(R.layout.skin_item, parent, false);
-			}
+		public long getChildId(int group, int child) {
+			return child;
+		}
 
-			SkinInfo skin = getItem(position);
-			TextView text1 = (TextView)view.findViewById(android.R.id.text1);
-			text1.setText(skin.name);
-			TextView text2 = (TextView)view.findViewById(android.R.id.text2);
-			if (skin.purchased)
-				text2.setVisibility(View.GONE);
+		class Holder {
+			ImageView thumbnail;
+			TextView text1;
+			TextView text2;
+		}
+
+		public View getChildView(int group, int child, boolean isLastChild, View view, ViewGroup parent) {
+			Holder holder;
+			if (view != null)
+				holder = (Holder)view.getTag();
 			else {
-				text2.setText(R.string.not_purchased);
-				text2.setVisibility(View.VISIBLE);
+				holder = new Holder();
+				final LayoutInflater inflater =  LayoutInflater.from(m_context);
+				view = inflater.inflate(R.layout.skin_item, parent, false);
+				view.setTag(holder);
+				holder.thumbnail = (ImageView)view.findViewById(R.id.thumbnail);
+				holder.text1 = (TextView)view.findViewById(android.R.id.text1);
+				holder.text2 = (TextView)view.findViewById(android.R.id.text2);
 			}
-			Drawable thumbnail = skin.getThumbnail(m_context);
-			if (thumbnail != null) {
-				ImageView iv = (ImageView)view.findViewById(R.id.thumbnail);
-				iv.setImageDrawable(thumbnail);
+
+			Variant variant = getChild(group, child);
+			holder.text1.setText(variant.name);
+			if (variant.detail.purchased)
+				holder.text2.setVisibility(View.GONE);
+			else {
+				holder.text2.setText(R.string.not_purchased);
+				holder.text2.setVisibility(View.VISIBLE);
 			}
+			Drawable thumbnail = variant.detail.getThumbnail(m_context);
+			if (thumbnail != null)
+				holder.thumbnail.setImageDrawable(thumbnail);
+
 			return view;
 		}
 
-		public int getCount() {
-			return m_showAll ? m_skins.size() : m_purchased.size();
+		public int getChildrenCount(int group) {
+			return m_skins.get(group).variants.size();
 		}
 
-		public SkinInfo getItem(int position) {
-			return m_showAll ? m_skins.get(position) : m_purchased.get(position);
+		public Skin getGroup(int group) {
+			return m_skins.get(group);
 		}
 
-		public long getItemId(int position) {
-			return position;
+		public int getGroupCount() {
+			return m_skins.size();
+		}
+
+		public long getGroupId(int group) {
+			return getGroup(group).id;
+		}
+
+		public View getGroupView(int group, boolean isExpanded, View view, ViewGroup parent) {
+			TextView text;
+			if (view != null)
+				text = (TextView)view.getTag();
+			else {
+				final LayoutInflater inflater =  LayoutInflater.from(m_context);
+				view = inflater.inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+				text = (TextView)view.findViewById(android.R.id.text1);
+				view.setTag(text);
+			}
+
+			Skin skin = getGroup(group);
+			text.setText(skin.name);
+
+			return view;
+		}
+
+		public boolean hasStableIds() {
+			return false;
+		}
+
+		public boolean isChildSelectable(int group, int child) {
+			return true;
 		}
 	}
 
@@ -201,8 +340,12 @@ public class SkinListActivity extends ListActivity {
 
 		@Override
 		protected void onResult(List<SkinInfo> result) {
-			if (result != null)
-				m_adapter.setSkins(result);
+			if (result == null)
+				return;
+
+			m_skins = reconstruct(result);
+			m_purchased = filterPurchased(m_skins.values());
+			m_adapter.setSkins(m_purchased);
 		}
 	}
 
@@ -224,8 +367,11 @@ public class SkinListActivity extends ListActivity {
 
 		@Override
 		protected void onResult(Boolean result) {
-			if (result)
-				m_adapter.notifyDataSetChanged();
+			if (result) {
+				boolean showAll = isShowAll();
+				m_purchased = filterPurchased(m_skins.values());
+				refresh(showAll);
+			}
 			else
 				Toast.makeText(SkinListActivity.this, R.string.faile_buying, Toast.LENGTH_SHORT).show();
 		}

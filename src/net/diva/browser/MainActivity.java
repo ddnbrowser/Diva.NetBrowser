f8e552a7ecee2df3bf328c3b7a07ab8c71e80e31
DdN Browser <ddnbrowser@gmail.com>
2011-10-13 21:50:53 +0900

    DIVA.NETメニューにランクインリストを追加

--- src/net/diva/browser/MainActivity.java
+++ src/net/diva/browser/MainActivity.java
@@ -131,6 +131,9 @@ public class MainActivity extends TabActivity implements TabHost.OnTabChangeList
 		case R.id.item_statistics:
 			WebBrowseActivity.open(this, "/divanet/pv/statistics/");
 			break;
+		case R.id.item_ranking_list:
+			WebBrowseActivity.open(this, "/divanet/ranking/list/0");
+			break;
 		case R.id.item_game_settings: {
 			Intent intent = new Intent(getApplicationContext(), CommonConfigActivity.class);
 			startActivity(intent);

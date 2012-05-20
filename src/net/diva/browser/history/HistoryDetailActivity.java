181e79d99549203ad5df88f9353cea7f7db9a057
DdN Browser <ddnbrowser@gmail.com>
2012-05-20 14:48:09 +0900

    画像による共有機能を削除
    リザルトピクチャー機能と絡めて再度実装予定

--- src/net/diva/browser/history/HistoryDetailActivity.java
+++ src/net/diva/browser/history/HistoryDetailActivity.java
@@ -1,10 +1,6 @@
 package net.diva.browser.history;
 
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.OutputStream;
-import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,24 +13,14 @@ import net.diva.browser.model.MusicInfo;
 import net.diva.browser.util.DdNUtil;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
+import android.content.ContentUris;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.graphics.Canvas;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.provider.MediaStore.Images;
-import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -47,16 +33,9 @@ import android.widget.TextView;
  *
  */
 public class HistoryDetailActivity extends Activity {
-
-	private static final String PACKAGE_NAME = "net.diva.browser";
-	private static final String OUT_STRAGE_IMAGE_DIR = "/" + PACKAGE_NAME + "/files";
-	private static final String LOCAL_STRAGE_IMAGE_DIR = "/data/data/" + PACKAGE_NAME + "/files";
-	private static final String IMAGE_FILE_NAME_HEADER = "DdNB_";
-
 	private HistoryStore m_store;
 	private History m_history;
 	private MusicInfo m_music;
-	private String m_localFilePath;
 
 	private Holder h;
 
@@ -66,16 +45,6 @@ public class HistoryDetailActivity extends Activity {
 		setContentView(R.layout.history_detail);
 
 		m_store = HistoryStore.getInstance(this);
-
-		for(File file : new File(LOCAL_STRAGE_IMAGE_DIR).listFiles()){
-			if(file.getName().matches("DdNB_.+\\.jpg")){
-				getContentResolver().delete(
-						MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-						MediaStore.Images.Media.DATA + " = ?",
-						new String[]{file.getPath()});
-				file.delete();
-			}
-		}
 
 		Intent intent = getIntent();
 		m_history = m_store.getPlayHistory(intent.getLongExtra("history_id", 0));
@@ -111,7 +80,7 @@ public class HistoryDetailActivity extends Activity {
 			lock(false);
 			break;
 		case R.id.item_share:
-			shareOtherApp(screenCapture());
+			share();
 			break;
 		case R.id.item_delete:
 			deleteHistory();
@@ -122,153 +91,27 @@ public class HistoryDetailActivity extends Activity {
 		return true;
 	}
 
-	private void shareOtherApp(Uri uri){
-		Intent send = new  Intent(Intent.ACTION_SEND);
-		send.setType("image/jpg");
-		send.putExtra(Intent.EXTRA_STREAM, uri);
-		send.putExtra(Intent.EXTRA_TEXT, createTweetMsg());
-		send.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-		send.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+	private void share(){
+		final long id = getIntent().getLongExtra("history_id", 0);
+		final Uri uri = ContentUris.withAppendedId(HistoryStore.URI_HISTORIES, id);
 
-		startActivity(send);
+		Intent intent = new  Intent(Intent.ACTION_SEND);
+		intent.setDataAndType(uri, "text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT, sharingMessage());
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
 	}
 
-	private Uri screenCapture(){
-
-		DisplayMetrics dm = getResources().getDisplayMetrics();
-		final float backup = dm.scaledDensity;
-		if(backup != 1.5f)
-			changeDensity(dm.scaledDensity * 540 / (dm.widthPixels < dm.heightPixels ? dm.widthPixels : dm.heightPixels));
-
-		Bitmap bitmap = createBitmap();
-
-		String filePath = LOCAL_STRAGE_IMAGE_DIR;
-		String fileName = IMAGE_FILE_NAME_HEADER + m_history.play_date + ".jpg";
-
-		//ファイルに保存
-		try {
-			byte[] w = bmp2data(bitmap, Bitmap.CompressFormat.JPEG, 80);
-			writeDataFile(fileName, w);
-
-			if(isMountedExSD()){
-				String outStragePath = Environment.getExternalStorageDirectory().getPath() + OUT_STRAGE_IMAGE_DIR;
-				File imgDir = new File(outStragePath);
-				if(!imgDir.exists())
-					if(!imgDir.mkdirs())
-						android.util.Log.e("", "make dir failed.");
-
-				m_localFilePath = LOCAL_STRAGE_IMAGE_DIR + "/" + fileName;
-				File localFile = new File(m_localFilePath);
-				File sdCardFile = new File(outStragePath + "/" + fileName);
-
-				if(copyFile(localFile, sdCardFile)){
-					localFile.delete();
-					filePath = sdCardFile.getParent();
-				}
-			}
-
-		} catch (Exception e) {
-		} finally {
-			changeDensity(backup);
-		}
-
-		ContentResolver cr = getContentResolver();
-
-		ContentValues values = new ContentValues();
-		values.put(Images.Media.TITLE, fileName);
-		values.put(Images.Media.DISPLAY_NAME, fileName);
-		values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis());
-		values.put(Images.Media.MIME_TYPE, "image/jpeg");
-		values.put(Images.Media.DATA, filePath + "/" + fileName);
-
-		Uri uri = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-		return uri;
-	}
-
-	private Bitmap createBitmap(){
-		final int[] captureSize = getResources().getIntArray(R.array.capture_size);
-		int captureHeight = captureSize[0];
-		int captureWidth = captureSize[1];
-		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-			captureHeight = captureSize[1];
-			captureWidth = captureSize[0];
-		}
-
-		Bitmap bmp = Bitmap.createBitmap(captureWidth, captureHeight, Config.ARGB_8888);
-		Canvas canvas = new Canvas(bmp);
-
-		View captureView = View.inflate(this, R.layout.history_detail, null);
-		new Holder(this, captureView).attach(m_history, m_music);
-
-		int measuredWidth = View.MeasureSpec.makeMeasureSpec(captureWidth, View.MeasureSpec.EXACTLY);
-		int measuredHeight = View.MeasureSpec.makeMeasureSpec(captureHeight, View.MeasureSpec.EXACTLY);
-
-		captureView.measure(measuredWidth, measuredWidth);
-		captureView.layout(0, 0, measuredWidth, measuredHeight);
-		captureView.invalidate();
-		captureView.draw(canvas);
-		return bmp;
-	}
-
-	private void changeDensity(float zoom){
-		DisplayMetrics dm = getResources().getDisplayMetrics();
-		dm.densityDpi = (int) (160 * zoom);
-		dm.density = zoom;
-		dm.xdpi = 160 * zoom;
-		dm.ydpi = 160 * zoom;
-		dm.scaledDensity = zoom;
-	}
-
-// 画面キャプチャ(現在画面に表示されているものをキャプチャする)
-//	private Bitmap createBitmap(){
-//		View view = this.getWindow().getDecorView();
-//		view.setDrawingCacheEnabled(true);
-//		Bitmap bitmap = view.getDrawingCache();
-//		if (bitmap == null)
-//			return null;
-//		return bitmap;
-//	}
-
-	private static byte[] bmp2data(Bitmap src, Bitmap.CompressFormat format, int quality) {
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		src.compress(format, quality, os);
-		return os.toByteArray();
-	}
-
-	private void writeDataFile(String name, byte[] w) throws Exception {
-		OutputStream out = null;
-		try {
-			out = openFileOutput(name, Context.MODE_WORLD_READABLE);
-			out.write(w, 0, w.length);
-			out.close();
-		} catch (Exception e) {
-			try {
-				if (out != null)
-					out.close();
-			} catch (Exception e2) {
-			}
-			throw e;
-		}
-	}
-
-	private boolean isMountedExSD(){
-		boolean ret = Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
-		return ret;
-	}
-
-	private boolean copyFile(File src, File dst) throws Exception {
-		Class<?> fileUtils = Class.forName("android.os.FileUtils");
-		Method m = fileUtils.getMethod("copyFile", File.class, File.class);
-		return (Boolean) m.invoke(fileUtils, src, dst);
-	}
-
-	private String createTweetMsg(){
+	private String sharingMessage(){
 		final int difficulty = m_music != null ? m_music.records[m_history.rank].difficulty : 0;
 		StringBuffer sb = new StringBuffer();
-		sb.append(m_history.music_title + " / ");
-		sb.append(String.format("%s ★%d / ", getResources().getStringArray(R.array.difficulty_names)[m_history.rank], difficulty));
-		sb.append(String.format("%d pts / ", m_history.score));
+		sb.append(m_history.music_title);
+		sb.append(" / ");
+		sb.append(getResources().getStringArray(R.array.difficulty_names)[m_history.rank]);
+		sb.append(" ★").append(difficulty);
+		sb.append(" / ");
+		sb.append(m_history.score).append(" pts");
+		sb.append(" / ");
 		sb.append(String.format("%d.%02d %% ", m_history.achievement/100, m_history.achievement%100));
 		sb.append("#DdNBrowser");
 		return sb.toString();
